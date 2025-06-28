378ff7447f3f14fd91dd5eb32cd400f3904ec2d0
MostafizFahim <98682669+MostafizFahim@users.noreply.github.com>
2025-06-28 19:40:07 +0600

    modified app.js

--- NexTrip/App.js
+++ NexTrip/App.js
@@ -27,9 +27,34 @@ import RateAndFeedbackScreen from "./screens/RateAndFeedbackScreen";
 import WalletScreen from "./screens/WalletScreen";
 import RideHistoryScreen from "./screens/RideHistoryScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import DriverProfileScreen from "./screens/DriverProfileScreen";
 import MapFullScreen from "./screens/MapFullScreen";
 import TripDetailsScreen from "./screens/TripDetailsScreen";
-// ...import all other global/modal screens as needed
+import EditProfileScreen from "./screens/EditProfileScreen";
+import PromoCodesScreen from "./screens/PromoCodesScreen";
+import FeedbackScreen from "./screens/FeedbackScreen";
+import NotificationsScreen from "./screens/NotificationsScreen";
+import NotificationsSettingsScreen from "./screens/NotificationsSettingsScreen";
+import LegalPrivacyPolicyScreen from "./screens/LegalPrivacyPolicyScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import HelpScreen from "./screens/HelpScreen";
+import FAQScreen from "./screens/FAQScreen";
+import ContactUsScreen from "./screens/ContactUsScreen";
+import AboutScreen from "./screens/AboutScreen";
+import InviteFriendsScreen from "./screens/InviteFriendsScreen";
+import EarningsScreen from "./screens/EarningsScreen";
+import AddCardScreen from "./screens/AddCardScreen";
+import AddVehicleScreen from "./screens/AddVehicleScreen";
+import UploadDocumentsScreen from "./screens/UploadDocumentsScreen";
+import UserManagementScreen from "./screens/UserManagementScreen";
+import DriverManagementScreen from "./screens/DriverManagementScreen";
+import AdminSupportScreen from "./screens/AdminSupportScreen";
+import AdminAnalyticsScreen from "./screens/AdminAnalyticsScreen";
+import AppInfoScreen from "./screens/AppInfoScreen";
+import BlockchainDetailsScreen from "./screens/BlockchainDetailsScreen";
+import LanguageSettingsScreen from "./screens/LanguageSettingsScreen";
+import TermsOfServiceScreen from "./screens/TermsOfServiceScreen";
+import RideShareScreen from "./screens/RideShareScreen";
 
 const Stack = createStackNavigator();
 
@@ -77,9 +102,49 @@ function MainApp() {
       <Stack.Screen name="Wallet" component={WalletScreen} />
       <Stack.Screen name="RideHistory" component={RideHistoryScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="DriverProfile" component={DriverProfileScreen} />
       <Stack.Screen name="MapFullScreen" component={MapFullScreen} />
       <Stack.Screen name="TripDetails" component={TripDetailsScreen} />
-      {/* Add all other global/modal screens here */}
+      <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+      <Stack.Screen name="PromoCodes" component={PromoCodesScreen} />
+      <Stack.Screen name="Feedback" component={FeedbackScreen} />
+      <Stack.Screen name="Notifications" component={NotificationsScreen} />
+      <Stack.Screen
+        name="NotificationsSettings"
+        component={NotificationsSettingsScreen}
+      />
+      <Stack.Screen
+        name="LegalPrivacyPolicy"
+        component={LegalPrivacyPolicyScreen}
+      />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+      <Stack.Screen name="Help" component={HelpScreen} />
+      <Stack.Screen name="FAQ" component={FAQScreen} />
+      <Stack.Screen name="ContactUs" component={ContactUsScreen} />
+      <Stack.Screen name="About" component={AboutScreen} />
+      <Stack.Screen name="InviteFriends" component={InviteFriendsScreen} />
+      <Stack.Screen name="Earnings" component={EarningsScreen} />
+      <Stack.Screen name="AddCard" component={AddCardScreen} />
+      <Stack.Screen name="AddVehicle" component={AddVehicleScreen} />
+      <Stack.Screen name="UploadDocuments" component={UploadDocumentsScreen} />
+      <Stack.Screen name="UserManagement" component={UserManagementScreen} />
+      <Stack.Screen
+        name="DriverManagement"
+        component={DriverManagementScreen}
+      />
+      <Stack.Screen name="AdminSupport" component={AdminSupportScreen} />
+      <Stack.Screen name="AdminAnalytics" component={AdminAnalyticsScreen} />
+      <Stack.Screen name="AppInfo" component={AppInfoScreen} />
+      <Stack.Screen
+        name="BlockchainDetails"
+        component={BlockchainDetailsScreen}
+      />
+      <Stack.Screen
+        name="LanguageSettings"
+        component={LanguageSettingsScreen}
+      />
+      <Stack.Screen name="TermsOfService" component={TermsOfServiceScreen} />
+      <Stack.Screen name="RideShare" component={RideShareScreen} />
     </Stack.Navigator>
   );
 }
